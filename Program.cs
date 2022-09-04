144f5867f701788ab150ac15c39e036cac1dc1ff
Olexiy Zakalyuzhnyy <alex.z.in.ua@gmail.com>
2022-09-04 21:37:57 +0300

    second commit

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ООП_ЛР1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World(ver2)");
             Console.ReadLine();
         }
     }
